26769c001d2f489e5bb78e801f051dc35f20cbb1
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2021-02-23 14:06:07 -0300

    Atividades springboot 02

---
+++ src/main/java/com/francisco/edivan/FcWeb02Application.java
@@ -0,0 +1,13 @@
+package com.francisco.edivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcWeb02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcWeb02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/francisco/edivan/FcWeb02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.edivan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcWeb02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
